c11d9696c83739dffb0d2ee6a19ae049894dbe42
codershangfeng <codershangfeng@gmail.com>
2020-08-09 09:23:37 +0800

    Initilize springboot

---
+++ src/main/java/me/codershangfeng/droolsworkbench/DroolsWorkbenchApplication.java
@@ -0,0 +1,13 @@
+package me.codershangfeng.droolsworkbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsWorkbenchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsWorkbenchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/codershangfeng/droolsworkbench/DroolsWorkbenchApplicationTests.java
@@ -0,0 +1,13 @@
+package me.codershangfeng.droolsworkbench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsWorkbenchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
